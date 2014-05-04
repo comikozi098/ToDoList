229e493738f58118ad420dcba6ae8a91935f5b60
Michael Kosmicki <Kozi098@gmail.com>
2014-05-04 14:42:15 -0400

    Initial Commit

---
+++ ToDoList/MLKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MLKAppDelegate.h
+//  ToDoList
+//
+//  Created by Michael Kosmicki on 5/4/14.
+//  Copyright (c) 2014 ___kozi___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MLKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Michael Kosmicki on 5/4/14.
+//  Copyright (c) 2014 ___kozi___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MLKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MLKAppDelegate class]));
+    }
+}
